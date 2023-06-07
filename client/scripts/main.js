1b7ed8c13feaf68f92bbfed515ce561dbf87cf60
Tijmen <tijmen.koetse@student.howest.be>
2023-06-07 19:27:44 +0200

    test

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,24 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const BASE_URL = import.meta.env.VITE_API_ENDPOINT || "";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+export const graphQLRequest = async (query, variables = {}) => {
+  const result = await fetch(`${BASE_URL}/api`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  }).then((res) => res.json());
+  if (!result.data) {
+    console.log(result);
+    throw new Error(result.errors[0].message);
+  }
+  return result;
+};
 
-setupCounter(document.querySelector('#counter'))
+export async function getWalls() {
+  const result = await graphQLRequest(
+    `query getWalls {
+      wallsEntries {
+        ... on walls_default_Entry {
+          id
+          title
+          placewall
+        }
+      }
+    }`
+    );
+
+  document.querySelector('.test').innerHTML = result.data.wallsEntries[0].title;
+  return result.data.wallsEntries;
+}
